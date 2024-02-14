992e4fec0a7d3e9327368bd3509b53dac2fdd2f2
Karim Zaafrani <85990373+Karim-Zaf@users.noreply.github.com>
2024-02-14 13:55:42 +0100

    Create A.cpp

---
+++ GYM/2023-2024 ICPC NERC (NEERC), North-Western Russia Regional Contest (Northern Subregionals)/A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+#ifndef ONLINE_JUDGE
+#include "dbg.cpp"
+#else
+#define dbg(...)
+#endif
+#define ll long long
+#define endl "\n"    
+#define fast ios::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL);
+#define pb push_back
+#define pp pop_back
+#define F first
+#define S second
+#define ordered_set tree<pair<ll, ll>, null_type, less<pair<ll, ll>>, rb_tree_tag,tree_order_statistics_node_update>
+const ll mod = 1e9+7 ;
+const ll inf = 1e18+500 ;
+ 
+using namespace std ;
+using namespace __gnu_pbds;
+ 
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;}
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);}
+ll inv(ll i) {if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod;} 
+
+
+void solve(){
+    int a[4] ; 
+    for(int i = 0 ; i < 4 ; i++) cin >> a[i] ; 
+    sort(a , a + 4) ;
+    cout << a[0] * a[2] << endl;
+}
+
+int main(){
+    fast 
+    
+    #ifndef ONLINE_JUDGE
+    freopen("inputf.txt","r",stdin);
+    freopen("outputf.txt","w",stdout);
+    #endif
+
+    ll t = 1 ; 
+    // cin >> t  ; 
+    while(t--) solve() ;
+    return 0;
+}  
