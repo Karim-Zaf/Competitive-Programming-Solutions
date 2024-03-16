ccd72fd2b933ea05bebfea3e9156a1eaf0418db0
Karim Zaafrani <85990373+Karim-Zaf@users.noreply.github.com>
2024-03-16 08:47:21 +0100

    Create B.cpp

---
+++ Tunisian Contests/TCPC/TCPC 2022/B.cpp
@@ -0,0 +1,53 @@
+/*    _   __   ___    _   ____    _    _        _____    ___     _____
+     | | / /  / _ \  | | |  _ \  | |  | |      |___  |  / _ \   |  ___|
+     | |/ /  | |_| | | | | |_| | |  \/  |         / /  | |_| |  | |__
+     |   /   |  _  | | | |  _ /  | |\/| |        / /   |  _  |  |  __|
+     | |\ \  | | | | | | | | \ \ | |  | |       / /__  | | | |  | |
+     |_| \_\ |_| |_| |_| |_| |_| |_|  |_|      |_____| |_| |_|  |_|      */
+#include<bits/stdc++.h>
+
+typedef long long ll;
+using namespace std;
+#ifndef ONLINE_JUDGE
+
+#include "debug.cpp"
+
+#else
+#define kar(...)
+#endif
+#define pb push_back
+#define F first
+#define S second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define int ll
+#define endl '\n'
+
+const int N = 3e5 + 30, mod = 1e9 + 7, MOD = mod, INF = 1e18 + 10;
+
+void Solve() {
+    int n;
+    cin >> n;
+    vector<int> v(n + 1), pref(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> v[i];
+        pref[i] = v[i] + pref[i - 1];
+    }
+    int q;
+    cin >> q;
+    while (q--) {
+        int x, y, m;
+        cin >> x >> y >> m;
+        cout << min(pref[m] + y, max(x, pref[n])) << endl;
+    }
+}
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(0), cin.tie(0);
+    int Test_case = 1;
+//    cin >> Test_case;
+    while (Test_case--) Solve();
+}
