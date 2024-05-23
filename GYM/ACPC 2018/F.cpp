9f1dfef19a045cfbd7655c5806d3dce2ec9ed033
Karim Zaafrani <85990373+Karim-Zaf@users.noreply.github.com>
2024-05-23 11:56:24 +0100

    Create F.cpp

---
+++ GYM/ACPC 2018/F.cpp
@@ -0,0 +1,58 @@
+/*    _   __   ___    _   ____    _    _        _____    ___     _____
+     | | / /  / _ \  | | |  _ \  | |  | |      |___  |  / _ \   |  ___|
+     | |/ /  | |_| | | | | |_| | |  \/  |         / /  | |_| |  | |__
+     |   /   |  _  | | | |  _ /  | |\/| |        / /   |  _  |  |  __|
+     | |\ \  | | | | | | | | \ \ | |  | |       / /__  | | | |  | |
+     |_| \_\ |_| |_| |_| |_| |_| |_|  |_|      |_____| |_| |_|  |_|      */
+#include<bits/stdc++.h>
+
+typedef long long ll;
+using namespace std;
+#ifndef ONLINE_JUDGE
+
+#include "debug.cpp"
+
+#else
+#define kar(...)
+#endif
+#define pb push_back
+#define F first
+#define S second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define int ll
+#define endl '\n'
+
+const int N = 3e5 + 30, mod = 1e9 + 7, INF = 1e18 + 10;
+
+void Solve() {
+    int n, m, x, y;
+    cin >> n >> m >> x >> y;
+
+    int ans = 0;
+    if (y == 0 && (m / x) < n) {
+        (cout << -1 << endl);
+        return;
+    } else if (y == 0) {
+        (cout << 0 << endl);
+        return;
+    }
+
+    while (true) {
+        int sum = (ans + m) / (x + y);
+        if (sum >= n) {
+            (cout << ans << endl);
+            return;
+        }
+        ans++;
+    }
+
+}
+
+int32_t main() {
+    freopen("mosalah.in", "r", stdin);
+    ios::sync_with_stdio(0), cin.tie(0);
+    int Test_case = 1;
+    cin >> Test_case;
+    while (Test_case--) Solve();
+}
