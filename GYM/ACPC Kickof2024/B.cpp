35ad1ff1184258dd53d6b6514d6cb8cc49057888
Karim Zaafrani <85990373+Karim-Zaf@users.noreply.github.com>
2024-06-14 19:26:41 +0100

    Create B.cpp

---
+++ GYM/ACPC Kickof2024/B.cpp
@@ -0,0 +1,60 @@
+/*    _   __   ___    _   ____    _    _        _____    ___     _____
+     | | / /  / _ \  | | |  _ \  | |  | |      |___  |  / _ \   |  ___|
+     | |/ /  | |_| | | | | |_| | |  \/  |         / /  | |_| |  | |__
+     |   /   |  _  | | | |  _ /  | |\/| |        / /   |  _  |  |  __|
+     | |\ \  | | | | | | | | \ \ | |  | |       / /__  | | | |  | |
+     |_| \_\ |_| |_| |_| |_| |_| |_|  |_|      |_____| |_| |_|  |_|      */
+#include<bits/stdc++.h>
+
+typedef long long ll;
+using namespace std;
+// #ifndef ONLINE_JUDGE
+// #include "debug.cpp"
+// #else
+// #define kar(...)
+// #endif
+#define pb push_back
+#define F first
+#define S second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define int ll
+#define endl '\n'
+
+const int N = 3e5 + 30, mod = 1e9 + 7, INF = 1e18 + 10;
+
+
+
+
+void Solve() {
+    int n;
+    cin >> n;
+    int sq = sqrt(n) + 5;
+    vector<int> nw;
+
+    int ans = INF;
+
+    for (int i = 0; i <= sq; i++) {
+        ans = min(ans, abs(i * i - n));
+        ans = min(ans, abs(2 * i * i - n));
+
+        int oth = abs(i * i - n);
+        oth = sqrt(oth);
+
+        for (int j = oth - 2; j <= oth + 2; j++) {
+            ans = min(ans, abs(j * j + i * i - n));
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int32_t main() {
+//    #ifndef ONLINE_JUDGE
+//        freopen("input.txt","r",stdin),freopen("output.txt","w",stdout);
+//    #endif
+    ios::sync_with_stdio(0), cin.tie(0);
+    int Test_case = 1;
+    cin >> Test_case;
+    while (Test_case--) Solve();
+}
